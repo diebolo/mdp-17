77564bd42578a637de9c42787c2345d933b4c929
Dielof van Loon <d.i.vanloon@student.tudelft.nl>
2024-06-05 14:22:58 +0200

    Mirte src update

---
+++ mirte_src/mirte-clean-robot/src/stop_for_obstacles.py
@@ -0,0 +1,14 @@
+import rospy
+
+def main():
+    """
+    Entry point of the program.
+    
+    Initializes the ROS node
+    """
+    rospy.init_node("stop_for_obstacles")
+
+
+if __name__ == "__main__":
+
+    main()
